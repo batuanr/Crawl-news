37a541cf58b92b9133225238614121292872ee66
batuanr <tuanpesi@gmail.com>
2021-10-13 15:13:00 +0700

    xong

---
+++ src/Regex.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+//            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("title=\"(.*?)\" href=\"/event/(.*?)\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(3));
+            }
+            Pattern o = Pattern.compile("title=\"(.*?)\"><div class=");
+
+            Matcher n = o.matcher(content);
+            while (n.find()) {
+                System.out.println(n.group(1));
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
